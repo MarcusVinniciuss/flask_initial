4a20af6a1796d42ffe583d6049b2fe45415a7899
Marcus Vinnicius <marcus.vinnicius02@hotmail.com>
2021-12-01 02:49:49 -0300

    Create models and schemas

--- models/book.py
+++ models/book.py
@@ -20,3 +20,23 @@ class BookModel(db.Model):
             'title': self.title,
             'pages': self.pages
         }
+    
+    @classmethod
+    def find_by_title(cls, title):
+        return cls.query.filter_by(title=title).first()
+    
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+    
+    def save_to_db(self, ):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_from_db(self, ):
+        db.session.delete(self)
+        db.session.commit()
